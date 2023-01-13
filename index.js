4f0a265cc45ef832c5a94dae83b289ebc0291692
Exulenood <119940832+Exulenood@users.noreply.github.com>
2023-01-13 16:17:15 +0100

    Importet bent and add Testing Site

---
+++ index.js
@@ -0,0 +1,10 @@
+import bent from 'bent';
+
+// ############# TESTING SITE #####################
+console.log('########## Testing Site Start #########');
+//
+//
+
+//
+//
+console.log('########## Testing Site End #########');
